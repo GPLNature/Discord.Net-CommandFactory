8cba9f449e7c10aebdd5fd20658400f3b2403096
gplnature <gplnature@gmail.com>
2021-11-02 22:51:36 +0900

    Add ParameterTypes

--- CommandFactory/ParameterChecker.cs
+++ CommandFactory/ParameterChecker.cs
@@ -11,11 +11,14 @@ namespace CommandFactory
     private static readonly ReadOnlyDictionary<Type, ApplicationCommandOptionType> Mapper = new(new Dictionary<Type, ApplicationCommandOptionType> {
         { typeof(string), ApplicationCommandOptionType.String },
         { typeof(int), ApplicationCommandOptionType.Integer },
+        { typeof(uint), ApplicationCommandOptionType.Integer },
+        { typeof(long), ApplicationCommandOptionType.Integer },
         { typeof(bool), ApplicationCommandOptionType.Boolean },
         { typeof(IUser), ApplicationCommandOptionType.User },
         { typeof(IGuildChannel), ApplicationCommandOptionType.Channel },
         { typeof(IRole), ApplicationCommandOptionType.Role },
         { typeof(IMentionable), ApplicationCommandOptionType.Mentionable },
+        { typeof(float), ApplicationCommandOptionType.Number },
         { typeof(double), ApplicationCommandOptionType.Number },
       });
     
